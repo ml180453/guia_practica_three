3233deaf5f140363132196ce016b4edb67ecb70d
Edwin Melara Landaverde <ML180453@alumno.udb.edu.sv>
2024-08-29 16:50:30 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace guia_practica_three
+{
+    public partial class Form1 : Form
+    {
+        enum Posicion
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
